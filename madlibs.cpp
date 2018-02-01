a334d602bfde9dcd7f72a3a97ca5c8faf7bdad12
mitcaizi <jling1@mail.csuchico.edu>
2018-01-31 18:36:48 -0800

    Man save cats

--- madlibs.cpp
+++ madlibs.cpp
@@ -1,9 +1,31 @@
-//Authors: 
+//Authors: Jingwen Ling
 #include <iostream>
 #include <string>
 
 int main()
 {
+      std::string adjective;
+      double number;
+      std::string noun;
+      char character;
+
+      std::cout<<"What is the adjective?";
+      std::cin>>adjective;
+      
+      std::cout<<"What is the number?";
+      std::cin>>number;
+      
+      std::cout<<"What is the noun?";
+      std::cin>>noun;
+
+      std::cout<<"What is the punctuation";
+      std::cin>>character;
+
+      std::cout<<"He found" << adjective << "cats on the street yesterday." <<std::endl;
+      std::cout<<"There were" << number << "cats." <<std::endl;
+      std::cout<<"They did not eat any" << noun << "." <<std::endl;
+      std::cout<<"He gave them some food" << character << "." <<std::endl;
+
 
   return 0;
 }
